7e40dd86876354b3bb5ccee3476398bcdacf0be4
Blezzar <berggrenj@hotmail.com>
2024-05-24 22:54:37 +0200

    common word empty red

--- src/test/test_parser.py
+++ src/test/test_parser.py
@@ -1,7 +1,14 @@
 import src.utils.FileParser as FileParser
+import pytest
 
 class TestFileParser:
     def test_get_most_common_word_normal_case(self):
         text = "one one two three"
         parser = FileParser.FileParser(text)
         assert parser.get_most_common_word() == "one"
+
+    def test_get_most_common_word_empty_text(self):
+        text = ""
+        parser = FileParser.FileParser(text)
+        with pytest.raises(ValueError):
+            parser.get_most_common_word()
